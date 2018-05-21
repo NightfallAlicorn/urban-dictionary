92785a6ebda510cb2011bee05ec2c739d2184678
RaidAndFade <RaidAndFade@users.noreply.github.com>
2018-05-21 14:35:34 -0400

    Getting rid of semicolons

--- urban-dictionary.js
+++ urban-dictionary.js
@@ -31,15 +31,15 @@ function get (url, callback) {
       rawData += buffer
     })
     result.on('end', function () {
-      var data = null;
-      var error = null;
+      var data = null
+      var error = null
       try {
-        data = JSON.parse(rawData);
+        data = JSON.parse(rawData)
       } catch (error) {
-        data = null; //incase somehow data got set to not null, this is more of a failsafe.
-        error = new Error('Failed to parse retrieved Urban Dictionary JSON.');
+        data = null //incase somehow data got set to not null, this is more of a failsafe.
+        error = new Error('Failed to parse retrieved Urban Dictionary JSON.')
       }
-      callback(error, data);
+      callback(error, data)
     })
   })
 }
