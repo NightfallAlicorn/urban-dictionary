4634a5a99685e92fbc142da1f48cee7b42146f19
NightfallAlicorn <nightfallthedragon@gmail.com>
2020-12-06 19:08:49 +0000

    Update on current work in-progress.

--- debug.js
+++ debug.js
@@ -5,44 +5,50 @@ const ud = require('./urban-dictionary')
 const id = 217456
 const word = 'test'
 
-ud.defid(id, (error, entry) => {
-  if (error) {
-    console.error(`defid (callback) - ${error.message}`)
-    return
-  }
+// TO-DO: Make this script more tidier
 
-  console.log(`defid (callback) - ${entry.word} - ${entry.definition.substring(0, 50)}`)
-}).then((result) => {
-  console.log(`defid (promise) - ${result.word} - ${result.definition.substring(0, 50)}`)
+/*
+ud.defid(id).then((result) => {
+  console.log(`defid (promise) - ${result.definition}`)
 }).catch((error) => {
   console.error(`defid (promise) - ${error.message}`)
 })
 
-ud.random((error, entry) => {
+ud.defid(id, (error, result) => {
   if (error) {
-    console.error(`term (callback) - ${error.message}`)
+    console.error(`defid (callback) - ${error.message}`)
     return
   }
+  console.log(`defid (callback) - ${result.definition}`)
+})
+*/
 
-  console.log(`random (callback) - ${entry.word} - ${entry.definition.substring(0, 50)}`)
-}).then((result) => {
-  console.log(`random (promise) - ${result.word} - ${result.definition.substring(0, 50)}`)
+ud.random(id).then((result) => {
+  console.log(`random (promise) - ${result[0].definition}`)
 }).catch((error) => {
   console.error(`random (promise) - ${error.message}`)
 })
 
-ud.term(word, (error, entries, tags, sounds) => {
+ud.random((error, result) => {
   if (error) {
-    console.error(`term (callback) - ${error.message}`)
-    console.error(error.code)
+    console.error(`random (callback) - ${error.message}`)
     return
   }
+  console.log(`random (callback) - ${result[0].definition}`)
+})
 
-  console.log(`term (callback) - ${entries[0].word} - ${entries[0].definition.substring(0, 50)}`)
-  console.log(`term (callback) - defid ${entries[0].defid}`)
-}).then((result) => {
-  const entries = result.entries
-  console.log(`term (promise) - ${entries[0].word} - ${entries[0].definition.substring(0, 50)}`)
+/*
+ud.term(noResultTest).then((results) => {
+  console.log(results[0].definition)
 }).catch((error) => {
   console.error(`term (promise) - ${error.message}`)
 })
+
+ud.term(noResultTest, (error, results) => {
+  if (error) {
+    console.error(`term (callback) - ${error.message}`)
+    return
+  }
+  console.log(results[0].definition)
+})
+*/
--- urban-dictionary.js
+++ urban-dictionary.js
@@ -2,253 +2,159 @@
 
 const http = require('http')
 const querystring = require('querystring')
+const utilities = require('util')
 
-var methods = {}
-var randomCache = []
+const promises = {}
+const callbacks = {}
+const methods = {}
 
-process.on('unhandledRejection', () => {
-  // This is a place holder to prevent missing Promise catch errors.
-  // Since the user may use a callback instead of a Promise object.
+/**
+ *
+ * @param {string} pathname The API pathname to use.
+ * @param {Object} query Optional. An object containing the query data.
+ */
+const get = (pathname, query) => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      host: 'api.urbandictionary.com',
+      method: 'GET'
+    }
+
+    if (!query) {
+      options.path = `/v0/${pathname}`
+    } else {
+      options.path = `/v0/${pathname}?${querystring.stringify(query)}`
+    }
+
+    const request = http.request(options, (response) => {
+      let data = ''
+
+      response.on('data', (chunk) => {
+        data += chunk
+      })
+
+      response.on('end', () => {
+        let result = null
+
+        try {
+          result = JSON.parse(data)
+          resolve(result)
+        } catch (error) {
+          result = null
+          reject(new Error('Failed to parse retrieved Urban Dictionary JSON.'))
+        }
+      })
+    })
+
+    request.on('error', (error) => {
+      const statusCode = error.status
+
+      if (statusCode === 500) {
+        reject(new Error('Unable to connect to Urban Dictionary API. Their servers may be temporary offline.'))
+      }
+    })
+
+    request.end()
+  })
+}
+
+const noResults = () => {
+  return new Error('No results founds.')
+}
+
+promises.autocompleteExtra = async (term) => {
+  if (typeof term !== 'string') {
+    throw new TypeError('term has to be a string.')
+  }
+
+  return get('autocomplete-extra', { term: term }).then((result) => {
+    if (!result.results[0]) {
+      throw noResults()
+    }
+    return result.results
+  })
+}
+
+promises.autocomplete = async (term) => {
+  if (typeof term !== 'string') {
+    throw new TypeError('term has to be a string.')
+  }
+
+  return get('autocomplete', { term: term }).then((result) => {
+    if (!result[0]) {
+      throw noResults()
+    }
+    return result
+  })
+}
+
+promises.defid = async (id) => {
+  if (typeof id !== 'number') {
+    throw new TypeError('id has to be a number')
+  }
+
+  return get('define', { defid: id }).then((result) => {
+    if (!result.list[0]) {
+      throw noResults()
+    }
+    return result.list[0]
+  })
+}
+
+promises.random = async () => {
+  return get('random').then((result) => {
+    if (!result.list[0]) {
+      throw noResults()
+    }
+    return result.list
+  })
+}
+
+promises.term = async (term) => {
+  if (typeof word !== 'string') {
+    throw new TypeError('term has to be a string.')
+  }
+
+  return get('define', { term: term }).then((result) => {
+    if (!result.list[0]) {
+      throw noResults()
+    }
+    return result.list
+  })
+}
+
+promises.wordsOfTheDay = async () => {
+  return get('words-of-the-day').then((result) => {
+    if (!result.list[0]) {
+      throw noResults()
+    }
+    return result.list
+  })
+}
+
+// Create callback versions of the promise methods
+/*
+Object.keys(promises).forEach((property) => {
+  callbacks[property] = utilities.callbackify(promises[property])
 })
 
-/**
- * Copies an object without it being linked to the original.
- * @param {object} object Original object.
- * @return {object} Deep copied object.
- */
-function deepCopy (object) {
-  return JSON.parse(JSON.stringify(object))
-}
+methods.defid = (id, callback) => { return (!callback ? promises.defid(id) : callbacks.defid(id, callback)) }
+methods.random = (callback) => { return (!callback ? promises.random() : callbacks.random(callback)) }
+methods.term = (word, callback) => { return (!callback ? promises.term(word) : callbacks.term(word, callback)) }
+*/
 
-/**
- * Retrieves a JSON parsed object.
- * @param {string} url The full URL to retrieve from.
- * @param {function(error, object):void} callback
- * * argument[0] {error} error
- * * argument[1] {object} Parsed JSON result.
- */
-function get (url, callback) {
-  // https://nodejs.org/dist/latest-v8.x/docs/api/http.html#http_http_get_options_callback
-  http.get(url, (result) => {
-    const contentType = result.headers['content-type']
-    const statusCode = result.statusCode
+Object.keys(promises).forEach((property) => {
+  // Create callbackify variations of the async function promises.
+  callbacks[property] = utilities.callbackify(promises[property])
 
-    let error
-
-    if (statusCode !== 200) {
-      error = new Error(`Unable to send request for definitions. Status code: '${statusCode}'`)
-      error.code = 'ERR_REQUEST_SEND'
-    } else if (contentType.indexOf('application/json') === -1) {
-      error = new Error(`Content retrieved isn't JSON. Content type: '${contentType}'`)
-      error.code = 'ERR_RESPONSE_NOT_JSON'
-    }
-
-    if (error) {
-      // Removes response data to clear up memory.
-      result.resume()
-      callback(error)
-      return
-    }
-
-    result.setEncoding('utf8')
-
-    let rawData = ''
-
-    result.on('data', (buffer) => {
-      rawData += buffer
-    })
-
-    result.on('end', () => {
-      let data = null
-      let error = null
-
-      try {
-        data = JSON.parse(rawData)
-      } catch (unusedError) {
-        // In case somehow the data got set to not null. This is more of a failsafe.
-        data = null
-        error = new Error('Failed to parse retrieved Urban Dictionary JSON.')
-        error.code = 'ERR_JSON_PARSE'
-      }
-
-      callback(error, data)
-    })
-  })
-}
-
-/**
- * Asynchronously obtain Urban Dictionary term entry by defid.
- * @param {number} id Definition entry to retrieve.
- * @param {function(error, object):promise} callback
- * * argument[0] {error} error
- * * argument[1] {object} entry
- * * * 'author' {string} Name of the poster.
- * * * 'current_vote' {string} Unknown. It only returns an empty string.
- * * * 'defid' {number} The unique definition entry ID.
- * * * 'definition' {string} The definition description.
- * * * 'example' {string} An example use of the definition.
- * * * 'permalink' {string} A shortened link to Urban Dictionary page of the definition.
- * * * 'thumbs_down' {number} Number of down votes.
- * * * 'thumbs_up' {number} Number of up votes.
- * * * 'word' {string} The word of the definition. Be aware that the casing might be different.
- * @return {promise} Returns a promise object containing the 'entry' property.
- */
-methods.defid = function defid (id, callback) {
-  if (typeof id !== 'number') {
-    let error = new TypeError('id has to be a number.')
-    error.code = 'ERR_ID_NOT_NUMBER'
-
-    throw error
-  }
-
-  return new Promise((resolve, reject) => {
-    const query = querystring.stringify({'defid': id})
-
-    get('http://api.urbandictionary.com/v0/define?' + query, (error, result) => {
-      if (error) {
-        if (typeof callback === 'function') {
-          callback(error)
-        }
-
-        reject(error)
-        return
-      }
-
-      if (!result.list[0]) {
-        let error = new Error('defid ' + id + " doesn't exist or has been deleted.")
-        error.code = 'ERR_DEFINITION_NOT_FOUND'
-
-        if (typeof callback === 'function') {
-          callback(error)
-        }
-
-        reject(error)
-        return
-      }
-
-      if (typeof callback === 'function') {
-        callback(null, result.list[0])
-      }
-
-      resolve(result.list[0])
-    })
-  })
-}
-
-/**
- * Asynchronously obtain a random definition from Urban Dictionary.
- *
- * When called, 10 definitions are downloaded at once and are output 1 at a time.
- * Once all 10 cache definitions are used, new ones are automatically downloaded.
- * @param {function(error, object):promise} callback
- * * argument[0] {error} error
- * * argument[1] {object} entry
- * * * 'author' {string} Name of the poster.
- * * * 'current_vote' {string} Unknown. It only returns an empty string.
- * * * 'defid' {number} The unique definition entry ID.
- * * * 'definition' {string} The definition description.
- * * * 'example' {string} An example use of the definition.
- * * * 'permalink' {string} A shortened link to Urban Dictionary page of the definition.
- * * * 'thumbs_down' {number} Number of down votes.
- * * * 'thumbs_up' {number} Number of up votes.
- * * * 'word' {string} The word of the definition. Be aware that the casing might be different.
- * @return {promise} Returns a promise object containing the 'entry' property.
- */
-methods.random = function random (callback) {
-  return new Promise((resolve, reject) => {
-    if (!randomCache[0]) {
-      get('http://api.urbandictionary.com/v0/random', (error, result) => {
-        if (error) {
-          if (typeof callback === 'function') {
-            callback(error)
-          }
-
-          reject(error)
-          return
-        }
-
-        randomCache = result.list
-        let entry = deepCopy(randomCache[0])
-        randomCache.splice(0, 1)
-
-        if (typeof callback === 'function') {
-          callback(null, entry)
-        }
-
-        resolve(entry)
-      })
+  // Create the public methods control flow for the callbacks and promises while forwarding the arguments.
+  methods[property] = (...args) => {
+    if (typeof args[args.length - 1] === 'function') {
+      return callbacks[property](...args)
     } else {
-      let entry = deepCopy(randomCache[0])
-      randomCache.splice(0, 1)
-
-      if (typeof callback === 'function') {
-        callback(null, entry)
-      }
-
-      resolve(entry)
+      return promises[property](...args)
     }
-  })
-}
-
-/**
- * Asynchronously obtain Urban Dictionary by term.
- * @param {number|string} word Definition to search for.
- * @param {function(error, array, array, array):promise} callback
- * * argument[0] {error} error
- * * argument[1] {array of object} entries
- * * * 'author' {string} Name of the poster.
- * * * 'current_vote' {string} Unknown. It only returns an empty string.
- * * * 'defid' {number} The unique definition entry ID.
- * * * 'definition' {string} The definition description.
- * * * 'example' {string} An example use of the definition.
- * * * 'permalink' {string} A shortened link to Urban Dictionary page of the definition.
- * * * 'thumbs_down' {number} Number of down votes.
- * * * 'thumbs_up' {number} Number of up votes.
- * * * 'word' {string} The word of the definition. Be aware that the casing might be different.
- * * argument[2] {array of string} tags Tags of related words.
- * * argument[3] {array of string} sounds Full link addresses to .mp3 and .wav files.
- * @return {promise} Returns a promise object containing entries, tags and sounds properties.
- */
-methods.term = function term (word, callback) {
-  if (typeof word !== 'string') {
-    let error = new TypeError('word has to be a string.')
-    error.code = 'ERR_WORD_NOT_STRING'
-
-    throw error
   }
-
-  return new Promise((resolve, reject) => {
-    const query = querystring.stringify({'term': word})
-
-    get('http://api.urbandictionary.com/v0/define?' + query, (error, result) => {
-      if (error) {
-        if (typeof callback === 'function') {
-          callback(error)
-        }
-
-        reject(error)
-        return
-      }
-
-      if (!result.list[0]) {
-        let error = new Error(word + ' is undefined.')
-        error.code = 'ERR_WORD_UNDEFINED'
-
-        if (typeof callback === 'function') {
-          callback(error)
-        }
-
-        reject(error)
-        return
-      }
-
-      if (typeof callback === 'function') {
-        callback(null, result.list, result.tags, result.sounds)
-      }
-
-      resolve({'entries': result.list, 'tags': result.tags, 'sounds': result.sounds})
-    })
-  })
-}
+})
 
 module.exports = methods
